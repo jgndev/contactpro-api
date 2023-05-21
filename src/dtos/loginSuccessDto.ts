9864f2db0f65f30efdd642ca2f86563681f1f3ba
Jeremy Novak <olepunchy@gmail.com>
2023-05-21 16:36:26 -0500

    Cleanup in authRouter

---
+++ src/dtos/loginSuccessDto.ts
@@ -0,0 +1,5 @@
+export interface LoginSuccessDto {
+    username: string;
+    email: string;
+    role: string;
+}
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -8,6 +8,7 @@ import {PrismaClient, User} from '@prisma/client'
 import {RegisterDto} from '../dtos/registerDto';
 import {LoginDto} from '../dtos/loginDto';
 import {RegisterSuccessDto} from "../dtos/registerSuccessDto";
+import {LoginSuccessDto} from "../dtos/loginSuccessDto";
 
 const router = express.Router();
 const prisma = new PrismaClient()
@@ -79,12 +80,16 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({message: "Invalid email or password"});
         }
 
-        const token = jwt.sign({id: user.id, role: user.role}, process.env.JWT_SECRET as string, {expiresIn: '1h'});
+        const successDto: LoginSuccessDto = {
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
 
         res.json({
             message: "Logged in successfully",
-            token,
-            user,
+            token: generateToken(user),
+            user: successDto,
         })
     } catch (error) {
         console.log(`Caught error logging in with email ${email}`);
