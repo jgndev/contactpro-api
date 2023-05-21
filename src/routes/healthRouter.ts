52076995137a19c2a563199592cde2ecbb2ca444
Jeremy Novak <olepunchy@gmail.com>
2023-05-21 16:41:58 -0500

    Cleanup commented code

--- src/routes/healthRouter.ts
+++ src/routes/healthRouter.ts
@@ -3,7 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log("What?");
     res.status(200).json({message: "Ok"});
 });
 
